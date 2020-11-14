be1cc5eb1ff782eeca06f5bc8ccce24f7227da0f
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-14 20:19:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,9 @@ puts loop_message_n_times("Hello Moon", 8)
 def output_array(array)
   count = 0 
   while count < array.length do
+    puts array[count]
+    count += 1 
+  end 
+end 
+
+puts output_array(["I", "Love", "Life"])
