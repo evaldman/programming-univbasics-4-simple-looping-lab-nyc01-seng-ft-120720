eb5510008344013523687435ccef7bf64b1fe0cb
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-14 20:05:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,14 @@
 def loop_message_five_times(message)
   count = 0 
- 
-  while count < 5 do 
+   while count < 5 do 
     puts message
     count += 1
   end
 end
+
+def loop_message_n_times(message, limit)
+  while count < limit do
+    puts message
+    count += 1 
+  end
+end 
